16d6cdce287555d6c9f28183de7600ded439096c
wenchijchang <heyyhey@hotmail.com>
2023-08-18 17:43:13 -0400

    Solved Task2

--- algorithms.py
+++ algorithms.py
@@ -3,15 +3,17 @@
 # The function should take the value in as a parameter and return a boolean value (True if even, False if odd).
 # Leave a comment above the function stating the time complexity.
 
+
 # O(1)
-def evenOrOdd(x):
+def even_or_odd(x):
     if x % 2 == 0:
         return True
     else:
         return False
 
-print (evenOrOdd(123))
-print (evenOrOdd(246))
+
+print(even_or_odd(123))
+print(even_or_odd(246))
 
 
 # Task 2: Less than 100
@@ -20,12 +22,17 @@ print (evenOrOdd(246))
 # Leave a comment above the function stating the time complexity.
 
 
+# 0(n)
+def is_lower_than_100(numbers):
+    for n in numbers:
+        if n <= 100:
+            return True
+        else:
+            return False
 
 
-
-
-
-
+print(is_lower_than_100([22, 13, 49]))
+print(is_lower_than_100([180, 14, 7]))
 
 
 # Task 3: Repeated Names
@@ -34,28 +41,21 @@ print (evenOrOdd(246))
 # Leave a comment above the function stating the time complexity.
 
 
-
-
-
-
-
-
-
-
 # Task 4: Sort List
 # Given a list of numbers, manually sort the list into ascending order (may not use built in .sort() method).
 # Suggested strategy:
-# Starting with the first two numbers, compare them to see which is larger. 
+# Starting with the first two numbers, compare them to see which is larger.
 # Place the lower number to the left and the higher number to the right.
 # Iterate through the list, checking each pair of numbers one at a time.
 # Repeat from the start until the entire list is sorted.
 # Leave a comment above the function stating the time complexity.
 
-def sort_list(arr):
 
+def sort_list(arr):
     # build algorithm here
 
     return arr
+
 
 unsorted = [6, 8, 3, 4, 7, 2]
 sorted = sort_list(unsorted)
